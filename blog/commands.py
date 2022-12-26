06e4c8371dae191e6abacbcfa847d0b26bfc696d
Sergey <smr.kolesnikov@gmail.com>
2022-12-26 14:18:47 +0500

    login

---
+++ blog/commands.py
@@ -0,0 +1,27 @@
+import click
+from werkzeug.security import generate_password_hash
+
+from Flask.blog.extensions import db
+
+
+@click.command('init-db')
+def init_db():
+    from Flask.wsgi import app
+
+    # import models for creating tables
+    from Flask.blog.models import User
+
+    # db.create_all(app=app)
+    db.create_all()
+
+
+@click.command('create-init-user')
+def create_init_user():
+    from Flask.blog.models import User
+    from Flask.wsgi import app
+
+    with app.app_context():
+        db.session.add(
+            User(email='name@example.com', password=generate_password_hash('test123'))
+        )
+        db.session.commit()
--- blog/user/views.py
+++ blog/user/views.py
@@ -1,29 +1,29 @@
 from flask import Blueprint, render_template
+from flask_login import login_required
 from werkzeug.exceptions import NotFound
 
+from Flask.blog.models import User
+
 user = Blueprint('user', __name__, url_prefix='/users', static_folder='../static')
-USERS = {
-    1: "James",
-    2: "Brian",
-    3: "Peter",
-}
 
 
 @user.route('/')
 def user_list():
-    return render_template('users/list.html', users=USERS)
+    users = User.query.all()
+    return render_template(
+        'users/list.html',
+        users=users,
+    )
 
 
 @user.route('/<int:pk>')
-def get_user_pk(pk: int):
-    user_pk = USERS.get(pk)
-    if user_pk:
-        return render_template('users/details.html', user_name=user_pk)
-    raise NotFound()
+@login_required
+def profile(pk: int):
+    selected_user = User.query.filter_by(id=pk).one_or_none()
+    if not selected_user:
+        raise NotFound(f"User #{pk} doesn't exist!")
 
-@user.route('/name/<int:pk>')
-def get_user_name(pk: int):
-    user_pk = USERS.get(pk)
-    if user_pk:
-        return user_pk
-    raise NotFound()
+    return render_template(
+        'users/profile.html',
+        user=selected_user,
+    )
